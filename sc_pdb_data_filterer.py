c6e14b4a9de4aa3e6590362e5fcabd7ceb025f4d
Tevfik Kiziloren <tevfik@ebi.ac.uk>
2023-05-30 14:45:26 +0100

    helper scripts

---
+++ sc_pdb_data_filterer.py
@@ -0,0 +1,25 @@
+import os.path
+import shutil
+from tqdm import trange
+import argparse
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("src", help="Source PDB Dataset Location")
+parser.add_argument("dest", help="Destination PDB Dataset Location, where filtered items wil be stored.")
+parser.add_argument("filter_file_path", help="Path of the file that includes the name of the proteins to be filtered.")
+args = parser.parse_args()
+
+SRC_SC_PDB_PATH = args["src"]
+DST_SC_PDB_PATH = args["dest"]
+FILTER_FILE_PATH = args["filter_file_path"]
+
+with open(FILTER_FILE_PATH) as fp:
+    lines = fp.readlines()
+    for item in trange(len(lines)):
+        line = lines[item]
+        line = line.replace("\n", "")
+        shutil.copytree(os.path.join(SRC_SC_PDB_PATH, line), os.path.join(DST_SC_PDB_PATH,line), dirs_exist_ok=True)
+
+
+
+
+
